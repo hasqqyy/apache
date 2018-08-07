e9994a97552d7d93099f199cbf6501765e7c2a0a
hasqqyy <1821975458@qq.com>
2018-08-07 21:44:12 +0800

    实现了空文件

--- bpp.js
+++ bpp.js
@@ -1,24 +1,41 @@
 //fs模块读取文件
-const http=require('http');
-const fs=require('fs');
-const path=require('path');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 //记录绝对路径
-let rootPath=path.join(__dirname,'WWW');
+let rootPath = path.join(__dirname, 'WWW');
 
 //创建服务器
-let server=http.createServer((request,response)=>{
-    //设置响应头的编码
-    response.setHeader('content-type','text/html;charset=utf-8');
-    response.end(`
-    洞庭张乐地，潇湘帝子游。
-云去苍梧野，水还江汉流。
-停骖我怅望，辍棹子夷犹。
-广平听方籍，茂陵将见求。
-心事俱己矣，江上徒离忧。
-    `);
+let server = http.createServer((request, response) => {
+    //生成地址
+    let targetPath = path.join(rootPath, request.url);
+    //判断路径是否存在
+    if (fs.existsSync(targetPath)) {
+        //判断是不是文件
+        response.end('hello')
+        let stats = fs.stat(targetPath);
+    } else {
+        //设置响应头的编码
+        response.setHeader('content-type', 'text/html;charset=utf-8');
+        //设置状态码
+        response.statusCode = 404;
+        response.end(`
+        <!DOCTYPE html>
+            <html lang="en">
+
+            <head></head>
+            <title>404 Not Found</title>
+            <body>
+                    <h1>Not Found</h1>
+                    <p>你请求的${request.url}不在服务器上哦,检查一下呗</p>
+            </body>
+
+            </html>
+        `)
+    }
 })
 //开启服务器
-server.listen(8848,('127.0.0.1'),()=>{
+server.listen(8848, ('127.0.0.1'), () => {
     console.log('开启成功');
 })
